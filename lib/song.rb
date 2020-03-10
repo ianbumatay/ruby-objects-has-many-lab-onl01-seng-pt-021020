0f4dc770f4f3c14eb1b25162bd93f104cb5ab238
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-10 13:03:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,11 +15,11 @@ class Song
     @@all
   end
 
-  def artist_name
-      if self.all.name == " "
-        nil
-      else
-        name
-      end
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
   end
 end
